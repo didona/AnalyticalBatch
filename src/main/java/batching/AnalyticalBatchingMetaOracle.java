e88f7b1005da8a300097920964fa6b7365b6c7d8
Diego Didona <didona@gsd.inesc-id.pt>
2014-03-29 16:18:43 +0000

    Push

---
+++ src/main/java/batching/AnalyticalBatchingMetaOracle.java
@@ -0,0 +1,21 @@
+package batching;
+
+import eu.cloudtm.autonomicManager.oracles.InputOracle;
+import eu.cloudtm.autonomicManager.oracles.Oracle;
+import eu.cloudtm.autonomicManager.oracles.OutputOracle;
+import eu.cloudtm.autonomicManager.oracles.exceptions.OracleException;
+
+/**
+ * @author Diego Didona
+ * @email didona@gsd.inesc-id.pt
+ * @since 29/03/14
+ */
+public class AnalyticalBatchingMetaOracle implements Oracle {
+
+   private final static AnalyticalFileBasedBatchingOracle realModel = new AnalyticalFileBasedBatchingOracle();
+
+   @Override
+   public OutputOracle forecast(InputOracle input) throws OracleException {
+      return realModel.forecast(input);
+   }
+}
